bec6bf3907970e304d5eecaa6c19df9a117fd68f
Manel <malotor@gmail.com>
2016-02-28 20:12:23 +0100

    bumb_version script updated

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
-
+pwd
 if [ -z "$1" ]
   then
     echo "No new version are provided"
     exit 1
 fi
 
-if [ -f VERSION ]
+if [ ! -f VERSION ]
   then
-    echo "Could not find a VERSION file"
-    exit 1
+    echo "Could not find a VERSION file. Creating one."
+    touch VERSION
 fi
 
 
-if [ -f CHANGELOG ]
+if [ ! -f CHANGELOG ]
   then
-    echo "Could not find a CHANGELOG file"
-    exit 1
+    echo "Could not find a CHANGELOG file. Creating one."
+    touch CHANGELOG
 fi
 
 CURRENT_VERSION=`cat VERSION`
 NEW_VERSION="$1"
+DATE=`date +"%Y-%m-%d"`
+
 echo "Current version : $CURRENT_VERSION"
 echo "New version : $NEW_VERSION"
-exit 1
+
 
 echo "Will set new version to be $NEW_VERSION"
 echo $NEW_VERSION > VERSION
-echo "Version $NEW_VERSION:" > tmpfile
-git log --pretty=format:" - %s" "v$CURRENT_VERSION"...HEAD >> tmpfile
+echo "v$NEW_VERSION ($DATE)" > tmpfile
+echo "-------------------" >> tmpfile
+git log --pretty=format:" * %s" "v$CURRENT_VERSION"...HEAD >> tmpfile
 echo "" >> tmpfile
 echo "" >> tmpfile
 cat CHANGELOG >> tmpfile
