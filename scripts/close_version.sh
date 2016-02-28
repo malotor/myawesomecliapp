254d00416c979ad75e496245d400f45838c430e3
Manel <malotor@gmail.com>
2016-02-28 22:35:46 +0100

    Fix close_version.sh

--- scripts/close_version.sh
+++ scripts/close_version.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-  then
-    echo "No new version are provided"
-    exit 1
-fi
-
 if [ ! -f VERSION ]
   then
-    echo "Could not find a VERSION file. Creating one."
-    touch VERSION
+    echo "Could not find a VERSION file."
 fi
 
 
 CURRENT_VERSION=`cat VERSION`
-NEW_VERSION="$1"
 
 git tag -a $CURRENT_VERSION -m "VERSION ${CURRENT_VERSION}"
